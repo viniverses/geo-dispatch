58c54995dae0c8f3a123215ec686b494138f43e4
Vinícius Silva <vinisuxx@outlook.com>
2026-01-29 21:26:04 -0300

    feat(prettier): add prettier config package

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  tabSize: 100,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
+
